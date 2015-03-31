7fe7b7e1657c120f136c24cbe3d8e95778ca8e79
Ross Hettel <rhettel@highground.com>
2015-03-31 15:33:43 -0500

    added link to order

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var Hipchat = require('node-hipchat'),
 HC.postMessage({
 	room: mainRoomId,
 	from: 'Chef Gus',
-	message: '(fooda) 15 Minutes left to order Fooda!',
+	message: '(fooda) 15 Minutes left to order Fooda! - https://select.fooda.com/my',
 	message_format: 'text',
 	notify: 1,
 	color: 'green'
