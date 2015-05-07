a2f0cee459592be78be44c016075a4c7dd58119d
Ross Hettel <rhettel@highground.com>
2015-05-07 11:54:49 -0500

    convert to spaces

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 "use strict";
 var Hipchat = require('node-hipchat'),
-	HC = new Hipchat(process.env.hipchat_token),
-	mainRoomId = '534478';  // HighGround room id
+     HC = new Hipchat(process.env.hipchat_token),
+     mainRoomId = '534478';  // HighGround room id
 
 HC.postMessage({
-	room: mainRoomId,
-	from: 'Chef Gus',
-	message: '(fooda) 15 Minutes left to order Fooda! - https://select.fooda.com/my',
-	message_format: 'text',
-	notify: 1,
-	color: 'green'
+     room: mainRoomId,
+     from: 'Chef Gus',
+     message: '(fooda) 15 Minutes left to order Fooda! - https://select.fooda.com/my',
+     message_format: 'text',
+     notify: 1,
+     color: 'green'
 }, function (data, err) {
-	console.log('sent message', err);
+     console.log('sent message', err);
 });
