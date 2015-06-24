f179e583e8822ecf17a274dee959dba041f7ed9d
Ross Hettel <rhettel@highground.com>
2015-06-24 10:42:10 -0500

    added more sayings, in proper french now

--- app.js
+++ app.js
@@ -2,15 +2,18 @@
 var superagent = require('superagent'),
     webhook = process.env.webhook,
     messages = [
-        'Bonjour, 15 Minutes ordre de gauche à Fooda!',
+        'Bonjour, Il ne reste que 15 minutes à demander le Fooda!',     //15 minutes to order Fooda
         'Quickly! Order zee Fooda! Mangez bien!',
-        '15 minutes left for Fooda! Bon appétit!'
+        '15 minutes left for Fooda! Bon appétit!',
+        'Compte à rebours à Fooda!',                                    //countdown to Fooda
+        'Le temps de demander le Fooda deminue rapidement!',            //the time to order fooda is quickly waning!
+        'Si vous voulez manger le Fooda, vous devez agir maintenant!'   //if you want to eat fooda, you must act quickly!
     ],
     payload = {
         username: 'Chef Gus',
         icon_emoji: ':chefgus:',
-        channel: '#highground',
-        text: '@channel - ' + messages[Math.floor(Math.random() * messages.length)] + "   :fooda:  <https://select.fooda.com/my|Order now!>"
+        channel: '#testing',
+        text: messages[Math.floor(Math.random() * messages.length)] + "   :fooda:  <https://select.fooda.com/my|Order now!>"
     };
 
 superagent.post(webhook, payload, function (err, res) {
