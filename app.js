c3d8f366809c7f3688b405d5cf728c383a797a94
Ross Hettel <rosshettel@gmail.com>
2015-11-21 16:45:52 -0600

    add worker

--- app.js
+++ app.js
@@ -4,30 +4,8 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     port = process.env.PORT || 8080,
     app = express(),
-    scheduler = require('node-schedule'),
-    contentBuilder = require('./contentBuilder'),
-    userNotificationStorage = require('./userNotificationStorage'),
-    logger = require('./logger'),
-    slackHandler = require('./slackHandler');
-
-scheduler.scheduleJob('45 9 * * 1-5', function () {
-    logger.debug('Posting 9:45 notification');
-    contentBuilder.buildPayload(function (err, payload) {
-        if (err) {
-            logger.error('Error getting payload', err);
-            return;
-        }
-        slackHandler.postToSlack(payload);
-    });
-});
-
-scheduler.scheduleJob('* * * * 1-5', function () {
-    logger.debug('Checking for user notifications');
-    //look up which users match this time
-    //userNotificationStorage.getUsersMatchingDate(new Date(), function)
-    //get an array of those users
-    //loop through array, build payload, post to slack
-});
+    slackHandler = require('./slackHandler'),
+    userNotificationStorage = require('./userNotificationStorage');
 
 app.use(bodyParser.json());
 
---
+++ worker.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var scheduler = require('node-schedule'),
+    contentBuilder = require('./contentBuilder'),
+    userNotificationStorage = require('./userNotificationStorage'),
+    logger = require('./logger'),
+    slackHandler = require('./slackHandler');
+
+scheduler.scheduleJob('45 9 * * 1-5', function () {
+    logger.debug('Posting 9:45 notification');
+    contentBuilder.buildPayload(function (err, payload) {
+        if (err) {
+            logger.error('Error getting payload', err);
+            return;
+        }
+        slackHandler.postToSlack(payload);
+    });
+});
+
+scheduler.scheduleJob('* * * * 1-5', function () {
+    logger.debug('Checking for user notifications');
+    //look up which users match this time
+    //userNotificationStorage.getUsersMatchingDate(new Date(), function)
+    //get an array of those users
+    //loop through array, build payload, post to slack
+});
