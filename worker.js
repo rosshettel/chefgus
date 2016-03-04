4d3621dd726b900791e38a03fb97430b6ba39f5c
Ross Hettel <rosshettel@gmail.com>
2016-03-04 11:11:42 -0600

    restart server with cluster

--- worker.js
+++ worker.js
@@ -1,48 +1,55 @@
 'use strict';
 
-var scheduler = require('node-schedule'),
-    moment = require('moment'),
-    contentBuilder = require('./contentBuilder'),
-    userStorage = require('./userStorage'),
-    logger = require('./logger'),
-    slackHandler = require('./slackHandler');
+var cluster = require('cluster'),
+    logger = require('./logger');
 
-logger.info('Worker started', moment().format('HH:mm'));
+if (cluster.isMaster) {
+    cluster.fork();
 
-process.on('uncaughtException', function (err) {
-    logger.error('Uncaught Exception!', err);
-    process.nextTick(function () {
-        process.exit(1);
+    cluster.on('exit', function (worker, code, signal) {
+        logger.error('Cluster exiting');
+        cluster.fork();
     });
-});
+}
 
-scheduler.scheduleJob('45 9 * * 1-5', function () {
-    logger.debug('Posting 9:45 notification');
-    contentBuilder.buildPayload(function (err, payload) {
-        if (err) {
-            logger.error('Error getting payload', err);
-            return;
-        }
-        slackHandler.postToSlack(payload);
+if (cluster.isWorker) {
+    var scheduler = require('node-schedule'),
+        moment = require('moment'),
+        contentBuilder = require('./contentBuilder'),
+        userStorage = require('./userStorage'),
+        slackHandler = require('./slackHandler');
+
+    logger.info('Worker started', moment().format('HH:mm'));
+
+    scheduler.scheduleJob('45 9 * * 1-5', function () {
+        logger.debug('Posting 9:45 notification');
+        contentBuilder.buildPayload(function (err, payload) {
+            if (err) {
+                logger.error('Error getting payload', err);
+                return;
+            }
+            slackHandler.postToSlack(payload);
+        });
     });
-});
 
 //scheduler.scheduleJob('* 0-9 * * 1-5', function () {
-scheduler.scheduleJob('* * * * *', function () {
-    //look up which users match this time
-    userStorage.getUsersMatchingDate(new Date(), function (err, users) {
-        if (users.length > 0) {
-            logger.debug('Found ' + users.length + ' users for ' + moment().format('HH:mm'));
+    scheduler.scheduleJob('* * * * *', function () {
+        //look up which users match this time
+        userStorage.getUsersMatchingDate(new Date(), function (err, users) {
+            if (users.length > 0) {
+                logger.debug('Found ' + users.length + ' users for ' + moment().format('HH:mm'));
 
-            users.forEach(function (user) {
-                contentBuilder.buildPayload(function (err, payload) {
-                    if (err) {
-                        logger.error('Error building payload', err);
-                        return;
-                    }
-                    slackHandler.postToSlack(payload);
+                users.forEach(function (user) {
+                    contentBuilder.buildPayload(function (err, payload) {
+                        if (err) {
+                            logger.error('Error building payload', err);
+                            return;
+                        }
+                        slackHandler.postToSlack(payload);
+                    });
                 });
-            });
-        }
+            }
+        });
     });
-});
+}
+
