565c49bc32e4b4a6d74c06696523f48f74981c8e
Ross Hettel <rosshettel@gmail.com>
2016-03-18 10:58:25 -0500

    accept input

--- notify.js
+++ notify.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var slackHandler = require('./slackHandler');
+var slackHandler = require('./slackHandler'),
+    defaultMessage = "Bonjour mes amies! I've added some new functionality I think you might enjoy! I can now remind you privately to order zee Fooda whenever you'd like. Just type `/chefgus 9am` into slack and I'll message you then. Bon appétit, Highground!",
+    message = process.argv[2] || defaultMessage;
 
 slackHandler.postToSlack({
     username: 'Chef Gus',
     icon_emoji: ':chefgus:',
     channel: process.env.MAIN_CHANNEL || '#testing',
-    text: "Bonjour mes amis! I've added some new functionality I think you might enjoy! I can now remind you privately to order zee Fooda whenever you'd like. Just type `/chefgus 9am` into slack and I'll message you then. Bon appétit, Highground!"
+    text: message
 });
